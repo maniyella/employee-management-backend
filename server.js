d5e91071ba44289af00d830f9e5adc1354e35a75
maneeshayella <maneeshayella@gmail.com>
2026-04-22 21:59:27 +0530

    changed port to 3000

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const employeeRoutes = require("./routes/employee.routes")
 app.use('/getemployees', employeeRoutes)
 app.use('/addemployee', employeeRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
