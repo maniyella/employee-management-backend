de5bcadcc98d1c3ed35ed658447e7cfc98690e4a
maneeshayella <maneeshayella@gmail.com>
2026-04-23 17:34:22 +0530

    inital data

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ console.group("before route")
 app.use('/getemployees', employeeRoutes)
 app.use('/addemployee', employeeRoutes)
 
+app.get('/', (req, res) => {
+  res.send('Backend running 🚀');
+}); 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
