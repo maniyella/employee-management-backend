b06d38f606ae374977f16319c3b1050e97bef9ea
maneeshayella <maneeshayella@gmail.com>
2026-04-29 15:51:29 +0530

    checking undefined valiues

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -3,6 +3,7 @@ const { getDb } = require('../db');
 exports.getAllEmployees =  async (req, res) => {
     const db = getDb();
     console.log("In CONTROLLER")
+    console.log("db", db)
     try {
         console.log("try block")
         const data = await db.collection('employees').find().toArray();
