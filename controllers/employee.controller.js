2e522f690883a1b0b2a1e99602ac1ed82645994d
maneeshayella <maneeshayella@gmail.com>
2026-05-04 20:51:08 +0530

    completed crud operations

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,4 +1,5 @@
 const { getDb } = require('../db');
+const { ObjectId } = require('mongodb');
 
 exports.getAllEmployees =  async (req, res) => {
     const db = getDb();
@@ -21,4 +22,50 @@ exports.addEmployee = async (req, res) => {
     } catch (err) {
     res.status(500).json(err);
   }
+}
+
+exports.updateEmployee = async (req, res) => {
+    const db = getDb();
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'Invalid ID' });
+    }
+    try {
+        const result = await db.collection('employees').updateOne(
+            { _id: new ObjectId(id) },
+            { $set: req.body }
+        );
+        res.json({
+            message: "Employee Updated",
+            updatedId: result.modifiedCount
+        });
+    } catch (err) {
+         res.status(500).json(err);
+    }
+}
+
+exports.deleteEmployee = async (req, res) => {
+    const db = getDb();
+    const id = req.params.id;
+    try {
+        const id = req.params.id;
+
+        // validate id
+        if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
+        }
+
+        const result = await db.collection('employees').deleteOne(
+            { _id: new ObjectId(id) }
+        );
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        res.json({ message: 'Employee deleted successfully' });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 }
--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -4,5 +4,7 @@ const employeeController = require('../controllers/employee.controller')
 // CRUD Routes
 router.get('/', employeeController.getAllEmployees);
 router.post('/', employeeController.addEmployee);
+router.post('/:id', employeeController.updateEmployee);
+router.delete('/:id', employeeController.deleteEmployee);
 
 module.exports = router;
